5e2c1788311095ef5f09c0062201f44d4e46771e
dropfen <boris.kotov@gmail.com>
2015-09-09 02:14:36 +0200

    Use DISQUS_SHORTNAME variable

--- disqus_loader.js
+++ disqus_loader.js
@@ -1,5 +1,5 @@
 DISQUS_LOADER = function() {
     var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-    dsq.src = '//' + Meteor.settings.public.disqus.shortname + '.disqus.com/embed.js';
+    dsq.src = '//' + DISQUS_SHORTNAME || Meteor.settings.public.disqus.shortname + '.disqus.com/embed.js';
     (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
 };
